6cc451638843c1486429022b06196fe6b05c7011
dearwhynot <dearwhynot@gmail.com>
2021-07-20 14:48:58 +0400

    first commit

---
+++ footer.php
@@ -0,0 +1,23 @@
+<footer class="footer" data-scroll="" data-scroll-repeat="" data-scroll-call="footer-inview">
+	<div class="footer__holder container">
+		<div class="footer__top-bar">
+			<ul class="footer-list list--tilda mb-10 md:mb-0 lg:w-1/5">
+				<li><a class="link" href="?instagram" target="_blank">Instagram</a></li>
+			</ul>
+			<div class="footer__nav-wrapper lg:flex-grow lg:px-10 lg:justify-center opacity-40">
+				<ul class="footer-list list--tilda mb-10 md:mb-0">
+					<li><a class="link" href="/index.html">Privacy Policy</a></li>
+					<li><a class="link" href="/index.html">Terms &amp; Conditions</a></li>
+				</ul>
+				<p class="footer__copy"><span class="footer__copy-text">Moonglade &copy;2021. All rights reserved</span></p>
+			</div>
+			<div class="hidden lg:block lg:w-1/5 lg:text-right opacity-40"><a class="link" href="https://obys.agency">Designed by obys</a></div>
+		</div>
+		<div class="footer__bottom-bar lg:hidden opacity-40">
+			<div class="flex justify-between items-center">
+				<a class="link" href="https://obys.agency">Designed by obys</a><span class="lg:hidden" data-smooth-scroll="top">( Back to top )</span>
+			</div>
+		</div>
+	</div>
+</footer>
+
